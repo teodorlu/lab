42647f9bc07f5bf7aa88e611b8263fd727e815ef
Teodor Heggelund <git@teod.eu>
2022-11-10 21:36:32 +0100

    link to latest version

--- src/heya.clj
+++ src/heya.clj
@@ -5,3 +5,7 @@
 (reductions * (range 1 10))
 
 ;; Write something here just to trigger a build.
+;;
+;; Go to the latest version of this notebook:
+;;
+;;   https://github.clerk.garden/teodorlu/clerk-stuff?update=1
