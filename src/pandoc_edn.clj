03ac948b22539b6bc14ca5288ae28b4e532a5930
Teodor Heggelund <git@teod.eu>
2022-12-27 22:59:35 +0100

    Provide commentary

--- src/pandoc_edn.clj
+++ src/pandoc_edn.clj
@@ -3,17 +3,46 @@
             [babashka.process]
             [clojure.java.shell]))
 
+;; # `pandoc-edn`: an intermediate representation for rich text
+;;
+;; Endgame: provide a nice, idiomatic Clojure interface to pandoc.
+;;
+;; Status: ended up spending a lot of time thinking about how to interface with
+;; pandoc. I shell out to Pandoc. But that pandoc dependency is implicit. So
+;; what do I do?
+;;
+;; At least I can ensure loose coupling on pandoc.
+;;
+;; =pandoc-edn= has an atom that controls how it shells out to pandoc:
+
 pandoc/!exec-fn
 
-(defn pandoc-exec-fn [s args]
+;; The library user has to set this themselves. Two libraries for shelling out
+;; to external processes are `clojure.java.shell` and `babashka.process`. Let's
+;; try both.
+
+;;
+;; with `babashka.process`:
+
+(defn pandoc-exec-fn-bb-process
+  "Shell out to pandoc with babashka.process"
+  [s args]
   (slurp (:out (apply babashka.process/process {:in s}
                       "pandoc" args))))
 
-(reset! pandoc/!exec-fn pandoc-exec-fn)
+(pandoc-exec-fn-bb-process "Good morning!" ["--from" "markdown" "--to" "html"])
 
-(comment
-  (pandoc-exec-fn "Good morning!" ["--from" "markdown" "--to" "html"])
+;; with `clojure.java.shell`:
 
-  (clojure.repl/doc babashka.process/process)
+(defn pandoc-exec-fn-clojure-java-shell
+  "SHell out to pandoc with clojure.java.shell"
+  [s args]
+  (let [cmd (concat ["pandoc"] args [:in s])]
+    (:out (apply clojure.java.shell/sh cmd))))
 
-  )
+(pandoc-exec-fn-clojure-java-shell "Good morning!" ["--from" "markdown" "--to" "html"])
+
+;; with both libraries side-by-side, I prefer `babashka.process`. No funny
+;; `concat`, no function keyword arguments. Option map, then args. Simple.
+
+(reset! pandoc/!exec-fn pandoc-exec-fn-bb-process)
