7c46fff4f5b1d9061e101aa92914c8f3f97c9f6c
Teodor Heggelund <git@teod.eu>
2022-12-27 22:27:16 +0100

    Configure pandoc backend

--- src/pandoc_edn.clj
+++ src/pandoc_edn.clj
@@ -1,16 +1,19 @@
 (ns pandoc-edn
-  (:require [teodorlu.pandoc.alpha2 :as pandoc]))
+  (:require [teodorlu.pandoc.alpha2 :as pandoc]
+            [babashka.process]
+            [babashka.process.pprint]))
 
 pandoc/!exec-fn
 
+(defn pandoc-exec-fn [s args]
+  (slurp (:out (apply babashka.process/process {:in s}
+                      "pandoc" args))))
+
+(reset! pandoc/!exec-fn pandoc-exec-fn)
+
 (comment
-  ;; perhaps
+  (pandoc-exec-fn "Good morning!" ["--from" "markdown" "--to" "html"])
 
-  pandoc/!binary-fn
-  pandoc/!exec
-  pandoc/!exec-fn
+  (clojure.repl/doc babashka.process/process)
 
-  ;; I think I like !exec-fn
   )
-
-(def pandoc (constantly nil))
