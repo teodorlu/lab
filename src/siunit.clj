a48071951d0881afcbf56f63f2fdb0de5b961636
Teodor Heggelund <git@teod.eu>
2023-01-25 20:42:18 +0100

    Add addition example

--- src/siunit.clj
+++ src/siunit.clj
@@ -3,25 +3,44 @@
   (:require [teodorlu.siunit.alpha1 :as siunit :refer [+ * /]]
             [teodorlu.siunit.alpha1.metric-prefix :refer [k m]]))
 
+;; In the spirit of SICMUtils, we try to make units work as you would want them with special operators.
+
+;; Units are maps from base units to their exponent.
+
 (def N {:kg 1 :m 1 :s -2})
 
-;; Since we coerce into SI Units, we can do funny stuff like this:
+;; We overload *, which means we can do something like this:
 
 (* 12 k N :m
    30 m :m)
 
-;; Or be more explicit:
+;; Or we can name arguments:
 
 (let [force (* 12 k N :m)
       length (* 30 m :m)]
   (* force length))
 
 ;; Let's ask for that number in kN.
+
 (let [force (* 12 k N)
       length (* 30 m :m)]
   (/
    (* force length)
    k N))
+
+;; Summation is allowed when operands are of the same unit:
+
+(+ (* 1 :m)
+   (* 1 m :m))
+
+;; but not when operands are of different units:
+
+(comment
+  (+ (* 1 :m)
+     (* 1 :kg)))
+;; 👆 throws an AssertionError
+
+;; (I find :m as meter, and m as milli to be a bit confusing above)
 
 ;; There's also a reader literal:
 
