82a49133d144bf1520c53a09459df432624cd67e
Teodor Heggelund <git@teod.eu>
2023-02-27 13:23:23 +0100

    Implement numerical 1D convolution

---
+++ src/convolution.clj
@@ -0,0 +1,31 @@
+(ns convolution
+  (:require [nextjournal.clerk :as clerk]))
+
+;; goal
+;;
+;; 1. Explain convolution
+;; 2. Display how convolution is computed using a grid —
+;;    will also let me learn some Clerk primitives
+;; 3. Show image transformations using convolution
+;;     1. Sharpen
+;;     2. Blur
+;;     3. Vertical change
+;;     4. Gradients and laplace equation
+
+(clerk/html [:hr])
+
+;; Q: Am I able to implement convolution?
+
+(defn convolute [u v]
+  (let [N (dec (+ (count u) (count v)))
+        collect (atom (vec (repeat N 0)))]
+    (doseq [i (range (count u))
+            j (range (count v))]
+      (swap! collect update (+ i j)
+             (fn [oldval]
+               (+ oldval (* (get u i) (get v j))))))
+    @collect))
+
+(clerk/table (for [[u v expected] [[[1 2 3] [1] [1 2 3]]
+                                   [[1 2 3] [10 20 30] [10 40 100 120 90]]]]
+               {'u u 'v v 'expected expected 'actual (convolute u v)}))
