4e9317fa714b29c34ed39d141c2a0c9c7857ff21
Teodor Heggelund <git@teod.eu>
2023-03-22 20:22:31 +0100

    Add two chrisn solutions

--- src/hypercube3.clj
+++ src/hypercube3.clj
@@ -2,7 +2,10 @@
   (:require
    [nextjournal.clerk :as clerk]
    [tech.v3.datatype :as d]
-   [tech.v3.datatype.functional :as f]))
+   [tech.v3.datatype.functional :as f]
+   [ham-fisted.api :as hamf])
+  (:import [ham_fisted Transformables]))
+
 
 (vec (d/make-reader :int32 10 idx))
 
@@ -19,6 +22,28 @@
         small-randoms (f/* dz (create-rand N))
         randoms-ordered (f/+ starting-points small-randoms)]
     (f/mean randoms-ordered)))
+
+(let [N 10]
+  (vec (.toArray (.doubles (java.util.Random.) N))))
+
+(import '[ham_fisted Transformables])
+
+(defn hamf-rand-reducer [^long n]
+  (reify
+    clojure.lang.Counted
+    (count [_this] n)
+    clojure.lang.IReduceInit
+    (reduce [_this rfn acc]
+      (let [rfn (ham_fisted.Transformables/toDoubleReductionFn rfn)
+            r (java.util.Random.)]
+        (loop [idx 0 acc acc]
+          (if (< idx n)
+            (recur (unchecked-inc idx)
+                   (.invokePrim rfn acc (.nextDouble r)))
+            acc))))))
+
+(let [N 10]
+  (vec (hamf/double-array (hamf-rand-reducer N))))
 
 (clerk/col
  (str "java version: " (System/getProperty "java.version"))
@@ -39,6 +64,17 @@
                       {:name "teodorlu3"
                        :create-range (fn [N] (d/make-reader :int32 N idx))
                        :create-rand (fn [N] (d/clone (d/make-reader :float32 N (rand))))}
+                      {:name "chrisn1 (r.doubles())"
+                       :description "java-random-doubles-array-randomness"
+                       :create-range (fn [N] (d/make-reader :int32 N idx))
+                       :create-rand (fn [N] (.toArray (.doubles (java.util.Random.) N)))
+                       }
+                      {:name "chrisn2 (hamf reducer)"
+                       :description "ham_fisted.Transformables/toDoubleReductionFn reducer randomness"
+                       :create-range (fn [N] (d/make-reader :int32 N idx))
+                       :create-rand (fn [N] (hamf/double-array (hamf-rand-reducer N)))
+                       }
+
                       ]]
           (let [N (Math/pow 10 6)
                 config (assoc config :N N)
