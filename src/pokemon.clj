d5f1bea8eec9f6a3401c3bec586d0ea132a5315d
Teodor Heggelund <git@teod.eu>
2023-03-22 22:07:57 +0100

    Reduce fluff

--- src/pokemon.clj
+++ src/pokemon.clj
@@ -3,10 +3,6 @@
    [nextjournal.clerk :as clerk]))
 
 ;; based on / inspired by ladymey's Tablecloth talk:
-;;
-;; https://www.youtube.com/watch?v=a0T_d_N7wbg / https://github.com/ladymeyy/tablecloth-talks-and-workshop
-
-;; <iframe width="560" height="315" src="https://www.youtube.com/embed/a0T_d_N7wbg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 (defn youtube-embed [youtube-id]
   (let [default-size {:width 560 :height 315}
@@ -19,3 +15,5 @@
                      :allowfullscreen true})]))
 
 (clerk/html (youtube-embed "a0T_d_N7wbg"))
+
+;; full details on github: https://github.com/ladymeyy/tablecloth-talks-and-workshop
