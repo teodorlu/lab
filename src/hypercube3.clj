c3fdabd4af9d91e88af70c178dcd293f54a139c6
Teodor Heggelund <git@teod.eu>
2023-03-22 12:53:08 +0100

    Display java version alongside perf table

--- src/hypercube3.clj
+++ src/hypercube3.clj
@@ -20,32 +20,32 @@
         randoms-ordered (f/+ starting-points small-randoms)]
     (f/mean randoms-ordered)))
 
-(System/getProperty "java.version")
-
-(clerk/table
- (into []
-       (for [config [{:name "teodorlu1"
-                      :create-range range
-                      :create-rand #(repeatedly % rand)}
-                     {:name "daslu1"
-                      :create-range (fn [N] (d/as-reader (range N)))
-                      :create-rand (fn [N] (d/make-reader :float32 N (rand)))}
-                     {:name "daslu2"
-                      :create-range (fn [N] (d/clone (d/as-reader (range N))))
-                      :create-rand (fn [N] (d/make-reader :float32 N (rand)))}
-                     {:name "teodorlu2"
-                      :create-range (fn [N] (d/make-reader :int32 N idx))
-                      :create-rand (fn [N] (d/make-reader :float32 N (rand)))}
-                     {:name "teodorlu3"
-                      :create-range (fn [N] (d/make-reader :int32 N idx))
-                      :create-rand (fn [N] (d/clone (d/make-reader :float32 N (rand))))}
-                     ]]
-         (let [N (Math/pow 10 6)
-               config (assoc config :N N)
-               {:keys [duration result]} (timed #(hypercube-sample-single-variable3-mean config))]
-           {"config" (:name config)
-            "avg(sample(N))" result
-            "duration (ms)" duration}))))
+(clerk/col
+ (str "java version: " (System/getProperty "java.version"))
+ (clerk/table
+  (into []
+        (for [config [{:name "teodorlu1"
+                       :create-range range
+                       :create-rand #(repeatedly % rand)}
+                      {:name "daslu1"
+                       :create-range (fn [N] (d/as-reader (range N)))
+                       :create-rand (fn [N] (d/make-reader :float32 N (rand)))}
+                      {:name "daslu2"
+                       :create-range (fn [N] (d/clone (d/as-reader (range N))))
+                       :create-rand (fn [N] (d/make-reader :float32 N (rand)))}
+                      {:name "teodorlu2"
+                       :create-range (fn [N] (d/make-reader :int32 N idx))
+                       :create-rand (fn [N] (d/make-reader :float32 N (rand)))}
+                      {:name "teodorlu3"
+                       :create-range (fn [N] (d/make-reader :int32 N idx))
+                       :create-rand (fn [N] (d/clone (d/make-reader :float32 N (rand))))}
+                      ]]
+          (let [N (Math/pow 10 6)
+                config (assoc config :N N)
+                {:keys [duration result]} (timed #(hypercube-sample-single-variable3-mean config))]
+            {"config" (:name config)
+             "avg(sample(N))" result
+             "duration (ms)" duration})))))
 
 (comment
   (clerk/clear-cache!))
