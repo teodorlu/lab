18e9cdac9251a424fdd6201c82dda53d769a2d13
Teodor Heggelund <git@teod.eu>
2023-03-24 23:03:52 +0100

    Try a macro for defining and viewing as histogram
    , fail

--- src/correlation_howto.clj
+++ src/correlation_howto.clj
@@ -25,3 +25,15 @@
 (def Y (mc/sample N))
 
 ;; now, how do we compute the correlation of X and Y?
+
+(defmacro defh [var & body]
+  `(do (def ~var ~@body)
+       (mc/histogram ~var)))
+
+(macroexpand-1 '(defh X (mc/sample 11)))
+
+(defh Z (mc/sample 100))
+
+(type Z)
+
+(type (defh W (mc/sample 10)))
