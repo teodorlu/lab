d346b5ddb21bf240a894f98a2d045724964935a6
Teodor Heggelund <git@teod.eu>
2023-03-24 22:51:56 +0100

    Make a histogram

--- src/correlation_howto.clj
+++ src/correlation_howto.clj
@@ -7,5 +7,20 @@
 ;; 2. Measure correlation
 ;; 3. Tweak sample method and remeasure correlation.
 
-(ns correlation-howto)
+(ns correlation-howto
+  (:require
+   [nextjournal.clerk :as clerk]))
 
+(defn histogram
+  "histogram with automatic buckets, simply insert a list of values"
+  [values]
+  (clerk/vl {:data {:values (for [v values] {:x v})}
+             :mark :bar
+             :encoding {:x {:bin true
+                            :field :x}
+                        :y {:aggregate :count}}
+             :embed/opts {:actions false}}))
+
+(histogram (->> (repeatedly 30 rand)
+                (map (partial * 10))
+                (map int)))
