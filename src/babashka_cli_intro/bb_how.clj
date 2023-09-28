95feaf56d3e3c741cb3ac9e13a6981eecc66b578
Teodor Heggelund <git@teod.eu>
2023-09-28 14:12:05 +0200

    bb howto 2

--- src/babashka_cli_intro/bb_how.clj
+++ src/babashka_cli_intro/bb_how.clj
@@ -1,18 +1,23 @@
 (ns babashka-cli-intro.bb-how)
 
-;; _hvordan jobber jeg med babashka-kode?_
+;; ## hvordan jobber jeg med babashka-kode?
 ;;
 ;; 1. Akkurat som Clojure-kode, i en REPL.
 ;; 2. Pluss at jeg noen ganger har et lokalt installert CLI (feks `neil-dev`)
 
-;; _hvordan strukturerer jeg babashka-prosjekter?_
+;; ## hvordan strukturerer jeg babashka-prosjekter?
 ;;
 ;; 1. Akkurat som Clojure-prosjekter, med deps.edn
 ;; 2. Pluss at jeg har en `bb.edn` som peker til `deps.edn` og sier "hent ting derfra".
 
-;; caveats:
+;; ## caveats
 ;;
 ;; 1. Jeg har ikke jobbet i svære Clojure-prosjekter
 ;; 2. Jeg har ikke jobbet med svære Clojure-kodebaser.
 ;;
 ;; Mine erfaringer er fra min bruk, det er lurt å tenke sjæl.
+
+;; ## la oss se hvordan dette funker i praksis.
+;;
+;; 1. Jeg har laget noen eksempler
+;; 2. Prøvd å lage en bash-variant og en Clojure-variant, for å sammenlikne.
