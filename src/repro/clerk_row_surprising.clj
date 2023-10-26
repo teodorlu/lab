5ca4e315740d669212626a056cd7e395d950d85c
Teodor Heggelund <git@teod.eu>
2023-10-26 14:49:04 +0200

    Repro surprising Clerk behavior

---
+++ src/repro/clerk_row_surprising.clj
@@ -0,0 +1,43 @@
+(ns repro.clerk-row-surprising
+  (:require [nextjournal.clerk :as clerk]))
+
+(def ^:private geometry-parameters
+  #{:h :b :s :t :r})
+
+(defn scale-by
+  "Shrink or expand a profile with a factor"
+  [profile f & args]
+  (->> profile
+       (filter (comp geometry-parameters key))
+       (map (fn [[param value]]
+              [param (apply f value args)]))
+       (into {})))
+
+(defn scale
+  "Shrink or expand a profile with a factor"
+  [profile factor]
+  (->> profile
+       (filter (comp geometry-parameters key))
+       (map (fn [[param value]]
+              [param (* value factor)]))
+       (into {})))
+
+(def ^:private ipe300
+  {:r 15, :wy 557, :s 7.1, :prefix "IPE", :wz 80.5, :h 300, :b 150, :iz 6.04, :t 10.7, :iy 83.6, :profile 300, :a 5.38})
+
+(def ^:private hea300
+  {:r 27, :wy 1260, :s 8.5, :prefix "HEA", :wz 421, :h 290, :b 300, :iz 63.1, :t 14, :iy 182.6, :profile 300, :a 11.2})
+
+;; This looks like a bug to me.
+;; Only ipe300 is shown.
+;; Perhaps I'm using it wrong.
+
+(clerk/row
+ (scale ipe300 0.5)
+ (scale hea300 0.5))
+
+;; The list viewer shows both:
+
+(list
+ (scale-by ipe300 * 0.5)
+ (scale-by hea300 * 0.5))
