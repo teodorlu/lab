f33324b2d18f18fd9432cbead23c9984f15e2488
Teodor Heggelund <git@teod.eu>
2023-10-28 15:04:58 +0200

    Vocabulary in its own section

--- src/steel_beam_design.clj
+++ src/steel_beam_design.clj
@@ -12,12 +12,14 @@
 ;; You're probably wondering why steel beams look like they do, and want an explanation for that in a nice, functional programming language.
 ;; You're in luck—that's what you'll be getting today!
 ;;
-;; First, let us establish our vocabulary.
+;; ## Vocabulary: cross section, web and flange
+;;
+;; First, let us define our vocabulary.
 ;;
 ;; This is a _steel beam_:
 
 (clerk/caption "Steel beam supporting the first floor of a house"
- (clerk/image (io/resource "steel_beam_design/I-Beam_002.jpg")))
+               (clerk/image (io/resource "steel_beam_design/I-Beam_002.jpg")))
 
 ;; Image [from Wikipedia][I-Beam_002.jpg-source], retreived 2023-10-28, licensed CC BY-SA 3.0.
 ;;
