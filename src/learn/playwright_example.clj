e1f299e6124392314573b708d75d211dd0d232f3
Teodor Heggelund <git@teod.eu>
2024-01-20 15:03:33 +0100

    now it compiles (thanks Ed!)

--- src/learn/playwright_example.clj
+++ src/learn/playwright_example.clj
@@ -6,16 +6,28 @@
 ;; - Using Playwright with the Java client.
 
 (ns learn.playwright-example
-  (:import [com.microsoft.playwright Playwright])
+  (:import (com.microsoft.playwright Playwright Page BrowserType
+                                     Browsertype$LaunchOptions
+                                     ))
   (:require
    [nextjournal.clerk :as clerk]))
 
 ;; Running this takes quite a few seconds!
-(def playwright (Playwright/create))
-(def browser (.launch (.webkit playwright)))
-(def page (.newPage browser))
+(defonce playwright-state (atom (Playwright/create))) ;; so we can (.close @playwright-state)
+(def playwright @playwright-state)
 
-page
+(import com.microsoft.playwright.BrowserType$LaunchOptions)
+(-> playwright
+    (.firefox)
+    (.launch
+     (-> (BrowserType$LaunchOptions.)
+         (.setHeadless false)
+         (.setSlowMo 50))))
+;; => "Unable to resolve classname: BrowserType$LaunchOptions"
+
+
+;; (def browser (.launch (.webkit playwright)))
+;; (def page (.newPage browser))
 
 
 
@@ -26,3 +38,12 @@ page
   (clerk/halt!)
 
   :rcf)
+
+(comment
+  ;; eksperimentering teodor
+
+  com.microsoft.playwright.BrowserType$LaunchOptions
+  ;; => com.microsoft.playwright.BrowserType$LaunchOptions
+
+  ;; Dette ser lovende ut!!!
+  )
