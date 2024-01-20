2c2ebab1ddc590897aac182c4356e3eee46eb263
Teodor Heggelund <git@teod.eu>
2024-01-20 14:22:29 +0100

    It does something
    - it does not crash
    - yet i cannot observe any behavior.

---
+++ src/learn/playwright_example.clj
@@ -0,0 +1,28 @@
+;; # A Playwright example
+;;
+;; In this document, I'll explore:
+;;
+;; - Java interop
+;; - Using Playwright with the Java client.
+
+(ns learn.playwright-example
+  (:import [com.microsoft.playwright Playwright])
+  (:require
+    [nextjournal.clerk :as clerk]))
+
+;; Running this takes quite a few seconds!
+(def playwright (Playwright/create))
+(def browser (.launch (.webkit playwright)))
+(def page (.newPage browser))
+
+page
+
+
+
+(comment
+
+  123
+
+  (clerk/halt!)
+
+  :rcf)
