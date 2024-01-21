570e5bc0d73570806e75cc8dec63ce6ef0a03659
Teodor Heggelund <git@teod.eu>
2024-01-21 18:39:57 +0100

    learn some java interpo

---
+++ src/learn/java_interop.clj
@@ -0,0 +1,25 @@
+(ns learn.java-interop)
+
+;; reading https://clojure-doc.org/articles/language/interop/
+
+(import java.awt.Point)
+
+;; Java field access
+
+(let [p (Point. 0 10)]
+  (.x p))
+;; => 0
+
+;; Setters
+
+(let [p (Point. 0 10)]
+  (set! (.-y p) 100)
+  (.y p))
+;; => 100
+
+;; Can use both (.x p) and (.-x p=)
+
+(let [p (Point. 0 10)]
+  (set! (.-y p) 100)
+  [(.y p) (.-y p)])
+;; => [100 100]
