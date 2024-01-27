863409cca0a34584ad59be3d3ffaef8d3e43449b
Teodor Heggelund <git@teod.eu>
2024-01-27 11:36:59 +0100

    Browse to pages

--- src/learn/playwright_2.clj
+++ src/learn/playwright_2.clj
@@ -1,2 +1,41 @@
 (ns learn.playwright-2
-  (:import (com.microsoft.playwright Playwright BrowserType$LaunchOptions)))
+  (:import (com.microsoft.playwright Playwright BrowserType$LaunchOptions))
+  (:require
+    [clojure.string :as str]
+    [nextjournal.clerk :as clerk]))
+
+(defonce playwright (Playwright/create))
+
+(defn firefox! [pw]
+  (-> pw
+      (.firefox)
+      (.launch
+       (-> (BrowserType$LaunchOptions.)
+           (.setHeadless false)))))
+
+(def firefox (firefox! playwright))
+
+(comment
+  (.close firefox)
+  (alter-var-root #'firefox (constantly (firefox! playwright)))
+  )
+
+;; are pages alive?
+
+(def urls
+  (for [name ["mikrobloggeriet.no"
+              "play.teod.eu"
+              "teod.eu"
+              "teod.eu/aphorisms"]]
+    {:filename (str (str/replace name #"/" "-") ".png")
+     :url (str "https://" name)}))
+
+(clerk/table urls)
+
+(comment
+  (doseq [url urls]
+    (let [page (.newPage firefox)]
+      (.navigate page url))
+    )
+
+  )
