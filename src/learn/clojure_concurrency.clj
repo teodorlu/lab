3db06526a27436f0f54b447309ed82db23687397
Teodor Heggelund <git@teod.eu>
2024-02-24 13:21:32 +0100

    Clojure concurrency

---
+++ src/learn/clojure_concurrency.clj
@@ -0,0 +1,6 @@
+;; # Clojure concurrency howto
+;;
+;; Problem statement: get hands-on experience with different approaches to doing
+;; concurrency with Clojure, then compare how those approaches feel in practice.
+
+(ns learn.clojure-concurrency)
