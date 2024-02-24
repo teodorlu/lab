72e22ce2084a284db0df3b8af559338e17ca55ee
Teodor Heggelund <git@teod.eu>
2024-02-24 13:24:24 +0100

    Sync an atom

--- src/learn/clojure_concurrency.clj
+++ src/learn/clojure_concurrency.clj
@@ -3,4 +3,14 @@
 ;; Problem statement: get hands-on experience with different approaches to doing
 ;; concurrency with Clojure, then compare how those approaches feel in practice.
 
-(ns learn.clojure-concurrency)
+(ns learn.clojure-concurrency
+  (:require [nextjournal.clerk :as clerk]))
+
+^::clerk/sync
+(defonce state1 (atom {}))
+
+@state1
+
+(comment
+  (swap! state1 update :counter (fnil inc 0))
+  )
