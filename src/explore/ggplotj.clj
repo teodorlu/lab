abdf6a1a1c3d283cdce69a31d2408863fc748897
Teodor Heggelund <git@teod.eu>
2024-02-25 20:42:02 +0100

    write some operations

---
+++ src/explore/ggplotj.clj
@@ -0,0 +1,17 @@
+(ns explore.ggplotj
+  (:refer-clojure :exclude [+]))
+
+;; PROTOCOLS AND STRUCTS!
+;;
+;; as long as we don't need to dispatch on two arguments---then we need multimethods / a "defgeneric"
+
+(defn + [])
+(defn ggplot [])
+(defn aes [])
+(defn geom-point [])
+
+
+(comment
+  +
+
+  )
