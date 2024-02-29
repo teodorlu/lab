cb65782af44188eb4afd3632b1a785726893dc03
Teodor Heggelund <git@teod.eu>
2024-02-29 21:48:09 +0100

    a ggplot idea

---
+++ src/learn/ggplot_idea.clj
@@ -0,0 +1,38 @@
+(ns learn.ggplot-idea
+  (:refer-clojure :exclude [+]))
+
+;; Idea: translate ggplot code mindlessly to records and protocols.
+;;
+;; Source: https://r4ds.hadley.nz/data-visualize
+;;
+;; ```r
+;; ggplot(
+;;   data = penguins,
+;;   mapping = aes(x = flipper_length_mm, y = body_mass_g)
+;; )
+;; ```
+
+(defprotocol IMapping
+  "A GGPlot mapping"
+  (mapping [this]))
+
+(defrecord Aes [x y]
+  "A GGPlot aestetic"
+  IMapping
+  (mapping [this] :TODO))
+
+(defn + [ggplot component] :TODO)
+
+(defprotocol IComponent
+  (component [this]))
+
+(defrecord GeomPoint []
+  IComponent
+  (component [this] :TODO))
+
+(defrecord GGPlot [data mapping environment])
+
+(comment
+
+
+  )
