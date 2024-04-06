88ccf98c852a4a0808b419d27730e039663b9d50
Teodor Heggelund <git@teod.eu>
2024-04-06 17:02:37 +0200

    provide and consume values

--- src/learn/queues_kevin.clj
+++ src/learn/queues_kevin.clj
@@ -11,3 +11,23 @@
   (a/<!! q)
 
   )
+
+(defn provide-values
+  "Puts two values on the queue"
+  [ctx]
+  (a/go
+    (a/>! (::q ctx) "Hello!")
+    (a/>! (::q ctx) "Hello, there!!!")))
+
+(defn consume-values [ctx]
+  [(a/<!! (::q ctx))
+   (a/<!! (::q ctx))])
+
+(comment
+  (def ctx {::q (a/chan)})
+
+  (provide-values ctx)
+
+  (consume-values ctx)
+
+  )
