3b77428ecd3d1032f91d428fb1e0aad9034dacc8
Teodor Heggelund <git@teod.eu>
2024-07-01 10:27:15 +0200

    Mermaid.js example

---
+++ src/mermaid_example.clj
@@ -0,0 +1,26 @@
+;; # A Mermaid.js example
+
+(ns mermaid-example
+  (:require
+    [nextjournal.clerk :as clerk]))
+
+;; Based on an example from the Clerk book:
+;;
+;; https://book.clerk.vision/#loading-libraries
+
+(def mermaid-viewer
+  {:transform-fn clerk/mark-presented
+   :render-fn '(fn [value]
+                 (when value
+                   [nextjournal.clerk.render/with-d3-require {:package ["mermaid@8.14/dist/mermaid.js"]}
+                    (fn [mermaid]
+                      [:div {:ref (fn [el] (when el
+                                             (.render mermaid (str (gensym)) value #(set! (.-innerHTML el) %))))}])]))})
+
+(clerk/with-viewer mermaid-viewer
+  "stateDiagram-v2
+    [*] --> Still")
+
+(clerk/with-viewer mermaid-viewer
+  "flowchart LR
+    markdown[\"(range 10)\"]-- cin -->worker-- cout -->id[\"Results in a vector\"]")
