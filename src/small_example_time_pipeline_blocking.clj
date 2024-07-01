b0579c1a16e6d9cb73afa6c9c5d7a361f1dd6793
Teodor Heggelund <git@teod.eu>
2024-07-01 13:30:24 +0200

    More pipeline and clerk

--- src/small_example_time_pipeline_blocking.clj
+++ src/small_example_time_pipeline_blocking.clj
@@ -2,7 +2,10 @@
   (:refer-clojure :exclude [time])
   (:require
    [clojure.core.async :as a]
-   [nextjournal.clerk :as clerk]))
+   [nextjournal.clerk :as clerk]
+   [java-time-literals.core]
+   [nextjournal.clerk.viewer :as cv])
+  (:import (java.time Duration)))
 
 {:nextjournal.clerk/visibility {:result :hide}}
 
@@ -52,5 +55,60 @@
        :workers workers
        :duration-millis duration-millis}))))
 
+;; java.time.Duration i stedet for tall i implisitt enhet
+
+(defn time2* [f]
+  (let [before (System/currentTimeMillis)
+        value (f)
+        after (System/currentTimeMillis)
+        duration-millis (- after before)]
+    {:value value :duration (Duration/ofMillis duration-millis)}))
+
+(defmacro time2
+  "An alternative to `clojure.core/time` suitable for notebooks"
+  [& body]
+  `(time2* (fn [] ~@body)))
+
+(defn pipeline-blocking-example2 [n workers]
+  (let [cin (a/to-chan! (range n))
+        cout (a/chan)
+        add-10-xf (map (partial (slow 100 +) 10))]
+    (time2 (a/pipeline-blocking workers cout add-10-xf cin)
+           (a/<!! (a/into [] cout)))))
+
+(Duration/ofMillis 311)
+
+(center
+ (clerk/table
+  (for [job [{:n 10 :workers 4}
+             {:n 20 :workers 3}]]
+    (let [{:keys [n workers]} job
+          duration (:duration (pipeline-blocking-example2 n workers))]
+      {:n n
+       :workers workers
+       :duration (format "%,6d ms" (.toMillis duration))}))))
+
+(require 'nextjournal.clerk.viewer)
+(require 'nextjournal.clerk.viewer)
+
+(def duration-ms-viewer-wip
+  {:transform-fn (nextjournal.clerk.viewer/update-val
+                  (fn [duration]
+                    (format "%,6d ms" (.toMillis duration))))})
+
+(clerk/table
+ (for [{:keys [n workers duration]}
+       [{:n 10 :workers 4 :duration (Duration/ofMillis 311)}
+        {:n 20 :workers 3 :duration (Duration/ofMillis 721)}
+        {:n 500 :workers 50 :duration (Duration/ofMillis 1051)}]]
+   {:n n
+    :workers workers
+    :duration (clerk/with-viewer duration-ms-viewer-wip duration)}))
+
+nextjournal.clerk.viewer/default-viewers
+nextjournal.clerk.viewer/add-viewers
+
+
 ^{:nextjournal.clerk/visibility {:code :hide}}
 (clerk/html [:div {:style {:height "50vh"}}])
+#_ (clerk/halt!)
