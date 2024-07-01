53f6f1f1839b8627edf60f79b86a716ba5e8abc6
Teodor Heggelund <git@teod.eu>
2024-07-01 11:36:24 +0200

    More mermaid diagrams

--- src/easy_parallellism_with_pipeline_blocking.clj
+++ src/easy_parallellism_with_pipeline_blocking.clj
@@ -184,6 +184,16 @@
 ;; worker functions are the components that do some work on the values between
 ;; channels. In our case, we have a simple pipeline with one step:
 
+^{:nextjournal.clerk/visibility {:result :hide :code :hide}}
+(def mermaid-viewer
+  {:transform-fn clerk/mark-presented
+   :render-fn '(fn [value]
+                 (when value
+                   [nextjournal.clerk.render/with-d3-require {:package ["mermaid@8.14/dist/mermaid.js"]}
+                    (fn [mermaid]
+                      [:div {:ref (fn [el] (when el
+                                             (.render mermaid (str (gensym)) value #(set! (.-innerHTML el) %))))}])]))})
+
 ;;
 ;;                        +------------------+
 ;;                        |                  |
@@ -191,6 +201,19 @@
 ;;                        |                  |
 ;;                        +------------------+
 
+^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+(comment
+  ;; @ruben: ditto for denne: velg hva du liker best av ascii og Mermaid.
+  ;;
+  ;; Jeg prøvde å få til akkurat samme graf som du har tegnet over, men klarte ikke å tegne med mermaid uten start- og sluttnode.
+  )
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/caption
+ "A normal core.async workflow: a worker taking items from an input channel, transforming the item, and putting the transformed item on an output channel."
+ (clerk/with-viewer mermaid-viewer
+   "flowchart LR
+    source-- channel in -->worker-- channel out -->sink"))
 
 ;; Let's write some code using the core.async library. First, we will define our
 ;; worker function. It will immediately start a go block, continually take values
@@ -267,20 +290,12 @@
   ;; Hva tenker du? Skal vi ta med noen sånne?
   )
 
-^{:nextjournal.clerk/visibility {:result :hide :code :hide}}
-(def mermaid-viewer
-  {:transform-fn clerk/mark-presented
-   :render-fn '(fn [value]
-                 (when value
-                   [nextjournal.clerk.render/with-d3-require {:package ["mermaid@8.14/dist/mermaid.js"]}
-                    (fn [mermaid]
-                      [:div {:ref (fn [el] (when el
-                                             (.render mermaid (str (gensym)) value #(set! (.-innerHTML el) %))))}])]))})
-
 ^{:nextjournal.clerk/visibility {:code :hide}}
-(clerk/with-viewer mermaid-viewer
-  "flowchart LR
-    source[\"(range 10)\"]-- cin -->worker[\"(pipeline-blocking 3 cout xf cin)\"]-- cout -->sink[\"[10, 11, 12, …]\"]")
+(clerk/caption
+ "pipeline-blocking with 3 worker threads"
+ (clerk/with-viewer mermaid-viewer
+   "flowchart LR
+    source[\"(range 10)\"]-- cin -->worker[\"(pipeline-blocking 3 cout xf cin)\"]-- cout -->sink[\"[10, 11, 12, …]\"]"))
 
 ;; Let's see if we can push it a bit...
 (let [cin (a/to-chan! (range 500))
@@ -288,11 +303,12 @@
   (time (a/pipeline-blocking 50 cout xf cin)
         (a/<!! (a/into [] cout))))
 
-
 ^{:nextjournal.clerk/visibility {:code :hide}}
-(clerk/with-viewer mermaid-viewer
-  "flowchart LR
-    source[\"(range 500)\"]-- cin -->worker[\"(pipeline-blocking 50 cout xf cin)\"]-- cout -->sink[\"[10, 11, 12, …]\"]")
+(clerk/caption
+ "Pipeline-blocking with 50 worker threads"
+ (clerk/with-viewer mermaid-viewer
+   "flowchart LR
+    source[\"(range 500)\"]-- cin -->worker[\"(pipeline-blocking 50 cout xf cin)\"]-- cout -->sink[\"[10, 11, 12, …]\"]"))
 
 ;; ...and that's it!
 ;; In my view, `pipeline-blocking` offers a lot of benefits. We don't have to manage worker functions, and
