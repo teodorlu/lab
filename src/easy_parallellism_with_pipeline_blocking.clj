d52c32d796fb99eb86a27409deacde3b87f6312d
Teodor Heggelund <git@teod.eu>
2024-07-06 18:12:32 +0200

    Shorter title, "Clojure" implied

--- src/easy_parallellism_with_pipeline_blocking.clj
+++ src/easy_parallellism_with_pipeline_blocking.clj
@@ -1,6 +1,6 @@
 ;; [..](https://play.teod.eu)
 ;;
-;; # Easy, explicit parallellism with `pipeline-blocking` in Clojure
+;; # Easy, explicit parallellism with `pipeline-blocking`
 ;;
 ;; If you want a `map` to go faster, you could use `pmap`.
 ;; But what if you need more control than `pmap` can give you?
